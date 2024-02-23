ee29f8b15e2753f326d5baacfb48d51dca66cae9
ryalinikhil <114085020+ryalinikhil@users.noreply.github.com>
2024-02-24 01:53:15 +0530

    Update AppDataModel.swift
    Adding a pop-up and progress View instead of crashing the app for Non-LiDAR Models.

--- Sources/USDZScanner/AppDataModel.swift
+++ Sources/USDZScanner/AppDataModel.swift
@@ -170,7 +170,11 @@ class AppDataModel: ObservableObject, Identifiable {
     private func startNewCapture() -> Bool {
         logger.log("startNewCapture() called...")
         if !ObjectCaptureSession.isSupported {
-            preconditionFailure("ObjectCaptureSession is not supported on this device!")
+            //preconditionFailure("ObjectCaptureSession is not supported on this device!")
+            // App will Show a POP-Up and Progress View instead of Crashing 
+            showLiDARNotAvailableAlert()
+                return false
+            
         }
 
         guard let folderManager = CaptureFolderManager() else {
@@ -238,7 +242,26 @@ class AppDataModel: ObservableObject, Identifiable {
         isObjectFlipped = false
         state = .ready
     }
+    private func showLiDARNotAvailableAlert() {
+        let alertController = UIAlertController(
+            title: "LiDAR Not Available",
+            message: "ObjectCaptureSession is not supported on this device.",
+            preferredStyle: .alert
+        )
 
+        alertController.addAction(UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil
+        ))
+
+        // Present the alert
+        if let viewController = UIApplication.shared.keyWindow?.rootViewController {
+            viewController.present(alertController, animated: true, completion: nil)
+        }
+    }
+
+    
     private func onStateChanged(newState: ObjectCaptureSession.CaptureState) {
         logger.info("ObjectCaptureSession switched to state: \(String(describing: newState))")
         if case .completed = newState {
